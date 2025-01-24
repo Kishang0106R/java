969be057b77e2ce6584027eb3eeb524722c6bfc9
Kishang0106R <158172017+Kishang0106R@users.noreply.github.com>
2025-01-24 15:35:31 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public static  int gcd(int a, int b){
+    int result = Math.min(a,b);
+    while(result>0){
+        if(a%result ==0 && b%result == 0){
+            break;
+        }
+        result--;
+    }
+    return result;
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number: ");
+    int a= sc.nextInt();
+    System.out.println("enter another number: ");
+    int b=sc.nextInt();
+    System.out.println("GCD of "+a+" and "+b+" is "+gcd(a,b));
+    sc.close();
+}

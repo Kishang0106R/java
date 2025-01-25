fcb16730df6382b1fa9552600561ba0d1d6dbee1
Kishang0106R <158172017+Kishang0106R@users.noreply.github.com>
2025-01-25 21:07:15 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class second {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter you array size: ");
+        int a = sc.nextInt();
+        int[] mark = new int[a];
+        for (int i = 0; i < a; i++) {
+            System.out.print("enter you array element " + i + ":");
+            mark[i] = sc.nextInt();
+        }
+        System.out.print("enter your which you want to find: ");
+        int b=sc.nextInt();
+        for (int i = 0; i < a; i++) {
+            if(mark[i]==b){
+                System.out.println("you element is fount at this position: "+i);
+            }
+        }
+        sc.close();
+    }
+}

ec723c1c6bd55c07cef835493c663ae9881654fe
Kishang0106R <158172017+Kishang0106R@users.noreply.github.com>
2025-01-25 21:09:57 +0530

    Update and rename second.java to searchingInArray.java

--- searchingInArray.java
+++ searchingInArray.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class second {
+public class searchingInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter you array size: ");

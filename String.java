378152691ebdf08fd09bfae72d44a4b9c94de15e
Kishang0106R <158172017+Kishang0106R@users.noreply.github.com>
2025-01-27 20:45:15 +0530

    Update and rename second.java to String.java

--- String.java
+++ String.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class second {
+public class String {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your first name: ");

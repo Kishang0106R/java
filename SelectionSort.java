fc85f52b05e67290d863c17f5cd46b62701db25d
Kishang0106R <158172017+Kishang0106R@users.noreply.github.com>
2025-02-01 13:13:38 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class second {
+public class SelectionSort {
     public  static  void  printarr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.println(arr[i]);
